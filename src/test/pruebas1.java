6df9cbf830a7f1c92925cbcc3b9bf70c705c970f
Luis Miguel <luismiguel.molina@campusfp.es>
2023-02-28 10:36:29 +0100

    cambio

--- src/test/pruebas1.java
+++ src/test/pruebas1.java
@@ -44,11 +44,7 @@ class pruebas1 {
 
 	@Test
 	void pruebasGenerales() {
-<<<<<<< HEAD
-		// comentario original
-=======
-		// comentario
->>>>>>> rama1
+		// rama2
 		// dado 2 arreglos de numeros la funcion sumar recibe ambos arreglos y suma de
 		// forma vectoraial
 		// ejemplo 1,2,3 y 4,5,6 la suma seria 5,7,9
